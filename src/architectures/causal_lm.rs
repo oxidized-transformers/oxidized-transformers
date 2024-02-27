a1278545da3b4eba49e983b663f0165febcf3910
Daniël de Kok <me@danieldk.eu>
2024-02-27 09:53:09 +0100

    Add `CausalLM` trait and transformer implementation

---
+++ src/architectures/causal_lm.rs
@@ -0,0 +1,87 @@
+use std::fmt::Debug;
+
+use candle_core::Tensor;
+use candle_nn::VarBuilder;
+
+use crate::architectures::{DecoderOutput, LayerOutputs};
+use crate::error::BoxedError;
+use crate::layers::attention::AttentionMask;
+
+/// Causal language model output.
+pub struct CausalLMOutput {
+    decoder_output: DecoderOutput,
+    logits: Tensor,
+}
+
+impl CausalLMOutput {
+    /// Create a causal language model output.
+    pub fn new(decoder_output: DecoderOutput, logits: Tensor) -> Self {
+        Self {
+            decoder_output,
+            logits,
+        }
+    }
+
+    /// Get the output of the decoder used by the causal language model.
+    pub fn decoder_output(&self) -> &DecoderOutput {
+        &self.decoder_output
+    }
+
+    /// Get the logits of the next predicted token.
+    ///
+    /// The logits are the unnormalized probabilities. Applying softmax to the
+    /// logits will give the probability distribution of the next token over the
+    /// vocabulary.
+    pub fn logits(&self) -> &Tensor {
+        &self.logits
+    }
+}
+
+impl LayerOutputs for CausalLMOutput {
+    fn layer_outputs(&self) -> &[Tensor] {
+        self.decoder_output.layer_outputs()
+    }
+
+    fn embedding_layer_output(&self) -> Option<&Tensor> {
+        self.decoder_output.embedding_layer_output()
+    }
+}
+
+/// Trait for building causal language models.
+pub trait BuildCausalLM: Debug {
+    /// Causal language model type.
+    type CausalLM: CausalLM;
+
+    /// Build a causal language model.
+    fn build(&self, vb: VarBuilder) -> Result<Self::CausalLM, BoxedError>;
+}
+
+/// Trait for causal language models.
+pub trait CausalLM {
+    /// Cache type for the causal language model.
+    type Cache;
+
+    /// Predict the next token using a causal language model.
+    ///
+    /// Returns the piece representations, cache, and logits of the next
+    /// predicted token.
+    ///
+    /// * `piece_ids` - Input sequence.
+    ///   *Shape:* `(batch_size, seq_len)`
+    /// * `attention_mask` - Attention mask. Sequence elements for which the
+    ///   corresponding mask element is set to `false` are ignored during
+    ///   attention calculation.
+    ///   *Shape:* `(batch_size, seq_len)`
+    /// * `cache` - Cache to avoid recomputing intermediate values.
+    /// * `positions` - Input positions.
+    ///   *Shape:* `(batch_size, seq_len)`
+    /// * `train` - Whether to train the layer.
+    fn forward_t(
+        &self,
+        piece_ids: &Tensor,
+        attention_mask: &AttentionMask,
+        cache: &mut Self::Cache,
+        positions: Option<&Tensor>,
+        train: bool,
+    ) -> Result<CausalLMOutput, BoxedError>;
+}
---
+++ src/models/transformer/causal_lm.rs
@@ -0,0 +1,128 @@
+use candle_core::{ModuleT, Tensor};
+use candle_nn::{linear_no_bias, VarBuilder};
+use snafu::{ResultExt, Snafu};
+
+use crate::architectures::{
+    BuildCausalLM, BuildDecoder, CausalLM, CausalLMOutput, Decoder, LayerOutputs,
+};
+use crate::error::BoxedError;
+use crate::kv_cache::KeyValueCache;
+use crate::layers::attention::AttentionMask;
+use crate::models::transformer::{TransformerDecoder, TransformerDecoderConfig};
+
+/// Transformer causal language model configuration.
+#[derive(Debug)]
+pub struct TransformerCausalLMConfig {
+    decoder: Box<dyn BuildDecoder<Decoder = TransformerDecoder>>,
+    hidden_size: usize,
+    n_pieces: usize,
+}
+
+impl TransformerCausalLMConfig {
+    /// Decoder.
+    ///
+    /// Default: `TransformerDecoderConfig`.
+    pub fn decoder(mut self, decoder: Box<dyn BuildDecoder<Decoder = TransformerDecoder>>) -> Self {
+        self.decoder = decoder;
+        self
+    }
+
+    /// Hidden size.
+    ///
+    /// Default: 4096.
+    pub fn hidden_size(mut self, hidden_size: usize) -> Self {
+        self.hidden_size = hidden_size;
+        self
+    }
+
+    /// Number of pieces in the output vocabulary.
+    ///
+    /// Default: 32000.
+    pub fn n_pieces(mut self, n_pieces: usize) -> Self {
+        self.n_pieces = n_pieces;
+        self
+    }
+}
+
+impl Default for TransformerCausalLMConfig {
+    fn default() -> Self {
+        Self {
+            decoder: Box::<TransformerDecoderConfig>::default(),
+            hidden_size: 4096,
+            n_pieces: 32000,
+        }
+    }
+}
+
+impl BuildCausalLM for TransformerCausalLMConfig {
+    type CausalLM = TransformerCausalLM;
+
+    fn build(&self, vb: VarBuilder) -> Result<Self::CausalLM, BoxedError> {
+        let decoder = Box::new(
+            self.decoder
+                .build(vb.push_prefix("decoder"))
+                .context(BuildDecoderSnafu)?,
+        );
+
+        Ok(TransformerCausalLM {
+            decoder,
+            output_embeddings: Box::new(
+                linear_no_bias(
+                    self.hidden_size,
+                    self.n_pieces,
+                    vb.push_prefix("output_embeddings"),
+                )
+                .context(BuildEmbeddingsSnafu)?,
+            ),
+        })
+    }
+}
+
+/// `TransformerCausalLM` errors.
+#[derive(Debug, Snafu)]
+pub enum TransformerCausalLMError {
+    #[snafu(display("Cannot build decoder"))]
+    BuildDecoder { source: BoxedError },
+
+    #[snafu(display("Cannot build output embeddings"))]
+    BuildEmbeddings { source: candle_core::Error },
+
+    #[snafu(display("Nothing to decode, the decoder does not have any layer outputs"))]
+    NoLayers,
+
+    #[snafu(display("Cannot compute logits for the output vocabulary"))]
+    ComputeLogits { source: candle_core::Error },
+}
+
+/// Transformer-based causal language model.
+pub struct TransformerCausalLM {
+    decoder: Box<dyn Decoder<Cache = KeyValueCache>>,
+    output_embeddings: Box<dyn ModuleT>,
+}
+
+impl CausalLM for TransformerCausalLM {
+    type Cache = KeyValueCache;
+
+    fn forward_t(
+        &self,
+        piece_ids: &Tensor,
+        mask: &AttentionMask,
+        cache: &mut Self::Cache,
+        positions: Option<&Tensor>,
+        train: bool,
+    ) -> Result<CausalLMOutput, BoxedError> {
+        let decoder_output = self
+            .decoder
+            .forward_t(piece_ids, mask, cache, positions, train)?;
+        let last_layer = decoder_output
+            .layer_outputs()
+            .last()
+            .ok_or(TransformerCausalLMError::NoLayers)?;
+        let logits = self
+            .output_embeddings
+            .forward_t(last_layer, train)
+            .context(ComputeLogitsSnafu)?;
+
+        Ok(CausalLMOutput::new(decoder_output, logits))
+    }
+}
