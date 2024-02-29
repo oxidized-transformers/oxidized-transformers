1ba32daefe144dc24df68ae77ed5b94a12360bbe
Daniël de Kok <me@danieldk.eu>
2024-02-29 11:52:14 +0100

    Let `Repo` directly return `PathBuf`
    This simplifies things a little at call sites. We might change things if
    we want to support streaming in the future, but that's not our focus now.

--- src/repository/hf_hub.rs
+++ src/repository/hf_hub.rs
@@ -1,12 +1,10 @@
 use std::collections::HashSet;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use hf_hub::api::sync::{Api, ApiError, ApiRepo};
 use hf_hub::{Repo as HuggingFaceRepo, RepoType as HuggingFaceRepoType};
 use snafu::{ResultExt, Snafu};
 
-use super::file::RepoFile;
-use super::local_file::LocalFile;
 use super::repo::Repo;
 use crate::error::BoxedError;
 
@@ -61,9 +59,7 @@ impl HfHubRepo {
 }
 
 impl Repo for HfHubRepo {
-    type File = HfHubFile;
-
-    fn file(&self, path: impl AsRef<Path>) -> Result<Self::File, BoxedError> {
+    fn file(&self, path: impl AsRef<Path>) -> Result<Option<PathBuf>, BoxedError> {
         let path_str = path.as_ref().to_string_lossy();
         if self.remote_path_exists(&path_str) {
             let local_path = self
@@ -71,46 +67,9 @@ impl Repo for HfHubRepo {
                 .get(&path_str)
                 .context(GetRemoteFileSnafu { path: path_str })?;
 
-            Ok(HfHubFile::local_file(local_path))
+            Ok(Some(local_path))
         } else {
-            Ok(HfHubFile::non_existent())
-        }
-    }
-}
-
-/// Wraps either a remote file on a Hugging Face Hub repository
-/// or a local file in the Hugging Face cache.
-pub enum HfHubFile {
-    Local(LocalFile),
-    NonExistent,
-}
-
-impl HfHubFile {
-    /// Create a new `HfHubFile` from a local file.
-    ///
-    /// * `local_path` - The path of the file on the local disk.
-    pub fn local_file(local_path: impl AsRef<Path>) -> Self {
-        Self::Local(LocalFile::new(local_path))
-    }
-
-    /// Create a new `HfHubFile` that represents a non-existent (remote) file.
-    pub fn non_existent() -> Self {
-        Self::NonExistent
-    }
-}
-
-impl RepoFile for HfHubFile {
-    fn local_path(&self) -> Option<impl AsRef<Path>> {
-        match self {
-            Self::Local(l) => l.local_path(),
-            _ => None,
-        }
-    }
-
-    fn exists(&self) -> bool {
-        match self {
-            Self::Local(l) => l.exists(),
-            _ => false,
+            Ok(None)
         }
     }
 }
