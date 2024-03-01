032d33a6589ef49702db2ff5e34fbd7cb3f500cc
Daniël de Kok <me@danieldk.eu>
2024-03-01 10:26:54 +0100

    Use approx/ndarray crates for tensor closeness tests
    We have used our own hand-rolled tensor assertion function. But
    from my experience with implementing various transformer architectures,
    this does not really work great.
    
    So, instead this change leverages the extensive approximate equality
    infrastructure that is already available to Rust.
    
    This change introduces the `IntoArrayD` trait for types that can be
    converted into `ArrayD` from `ndarray` and provides implementations
    for Candle tensors and a variety of arrays from `ndarray`.
    
    This trait is then used by a new `assert_tensor_eq` function to convert
    tensors to `ArrayD` and then compare them using `approx`.
    
    Future todo: nice diffing.

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,9 +2,56 @@ pub mod renaming_backend;
 
 #[cfg(test)]
 pub(crate) mod tests {
-    use candle_core::Tensor;
+    use std::error::Error;
+    use std::fmt::Debug;
 
-    pub(crate) fn assert_close(a: &Tensor, b: &Tensor, atol: f32) {
+    use approx::{assert_abs_diff_eq, AbsDiffEq};
+    use candle_core::{Tensor, WithDType};
+    use ndarray::{ArrayBase, ArrayD, DataOwned, Dimension};
+
+    // Like TryInto, but we need our own trait so that we can implement it
+    // for external types.
+    pub trait IntoArrayD<T> {
+        fn into_arrayd(self) -> Result<ArrayD<T>, Box<dyn Error>>;
+    }
+
+    impl<T> IntoArrayD<T> for Tensor
+    where
+        T: WithDType,
+    {
+        fn into_arrayd(self) -> Result<ArrayD<T>, Box<dyn Error>> {
+            (&self).into_arrayd()
+        }
+    }
+
+    impl<T> IntoArrayD<T> for &Tensor
+    where
+        T: WithDType,
+    {
+        fn into_arrayd(self) -> Result<ArrayD<T>, Box<dyn Error>> {
+            let data = self.reshape(((),))?.to_vec1()?;
+            Ok(ArrayD::from_shape_vec(self.shape().dims(), data)?)
+        }
+    }
+
+    impl<S, D, T> IntoArrayD<T> for ArrayBase<S, D>
+    where
+        D: Dimension,
+        S: DataOwned<Elem = T>,
+        T: Clone,
+    {
+        fn into_arrayd(self) -> Result<ArrayD<T>, Box<dyn Error>> where {
+            Ok(self.to_owned().into_dyn())
+        }
+    }
+
+    pub(crate) fn assert_tensor_eq<T>(a: impl IntoArrayD<T>, b: impl IntoArrayD<T>, epsilon: T)
+    where
+        T: AbsDiffEq<Epsilon = T> + Clone + Debug,
+    {
+        let a = a.into_arrayd().expect("Cannot convert array");
+        let b = b.into_arrayd().expect("Cannot convert array");
+
         assert_eq!(
             a.shape(),
             b.shape(),
@@ -13,19 +60,6 @@ pub(crate) mod tests {
             b.shape()
         );
 
-        let a: Vec<f32> = a
-            .flatten_all()
-            .unwrap()
-            .to_vec1()
-            .expect("Cannot convert Tensor to Vec<f32>");
-        let b: Vec<f32> = b
-            .flatten_all()
-            .unwrap()
-            .to_vec1()
-            .expect("Cannot convert Tensor to Vec<f32>");
-
-        for (x, y) in a.into_iter().zip(b.into_iter()) {
-            assert!((x - y).abs() < atol)
-        }
+        assert_abs_diff_eq!(a, b, epsilon = epsilon);
     }
 }
