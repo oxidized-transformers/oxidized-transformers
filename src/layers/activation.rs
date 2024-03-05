c811657ff51cd08bcf0e1c58adc3e958d502d7ac
Daniël de Kok <me@danieldk.eu>
2024-03-05 10:01:15 +0100

    Snake case activations
    This is in preparation for `gelu_new`, where we cannot simply lowercase
    the enum variant names anymore. Also add the `GeluNew` variant (used by
    ALBERT).

--- src/layers/activation.rs
+++ src/layers/activation.rs
@@ -8,31 +8,37 @@ use crate::layers::build_module::BuildModule;
 /// Activation functions.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[non_exhaustive]
-#[serde(rename_all = "lowercase")]
+#[serde(rename_all = "snake_case")]
 pub enum Activation {
     /// Gausian Error Linear Unit.
     ///
     /// See [Hendrycks and Gimpel, 2016](https://arxiv.org/abs/1606.08415).
-    GELU,
+    Gelu,
+
+    /// Gausian Error Linear Unit approximation.
+    ///
+    /// See [Hendrycks and Gimpel, 2016](https://arxiv.org/abs/1606.08415).
+    GeluNew,
 
     /// Rectified Linear Unit.
     ///
     /// See [Fukushima, 1969](https://ieeexplore.ieee.org/document/4082265).
-    ReLU,
+    Relu,
 
     /// Sigmoid Linear Unit.
     ///
     /// See [Hendrycks and Gimpel, 2016](https://arxiv.org/abs/1606.08415).
-    SiLU,
+    Silu,
 }
 
 impl BuildModule for Activation {
     fn build(&self, _vb: VarBuilder) -> Result<Box<dyn ModuleT>, BoxedError> {
         use Activation::*;
         Ok(match self {
-            GELU => Box::new(CandleActivation::Gelu),
-            ReLU => Box::new(CandleActivation::Relu),
-            SiLU => Box::new(CandleActivation::Silu),
+            Gelu => Box::new(CandleActivation::Gelu),
+            GeluNew => Box::new(CandleActivation::NewGelu),
+            Relu => Box::new(CandleActivation::Relu),
+            Silu => Box::new(CandleActivation::Silu),
         })
     }
 }
